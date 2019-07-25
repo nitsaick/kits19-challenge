b3fe798297f583439deca684e55540a726e23c3e
Nick Tsai <nick850810@gmail.com>
2019-07-25 15:12:33 +0800

    Fix __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,3 +1,2 @@
 from .kits19 import KiTS19
 from .kits19_roi import KiTS19_roi
-from .kits19_vol import KiTS19_vol
--- network/__init__.py
+++ network/__init__.py
@@ -1,5 +1,2 @@
 from .res_unet import ResUNet
 from .dense_unet_2d import DenseUNet2D
-from .dense_unet_3d import DenseUNet3D
-from .hybrid_net import HybridNet
-from .dense_aspp import DenseASPP
