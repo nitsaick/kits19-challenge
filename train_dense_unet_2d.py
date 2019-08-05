4654b8bdf6ac7f0aae60186a230361392621171f
Nick Tsai <nick850810@gmail.com>
2019-08-05 17:11:15 +0800

    Refactor train script

--- train_dense_unet_2d.py
+++ train_dense_unet_2d.py
@@ -11,8 +11,8 @@ from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from tqdm import tqdm
 
 import utils.checkpoint as cp
-from dataset import KiTS19_roi
-from dataset.transform import Compose, MedicalTransform2
+from dataset import KiTS19
+from dataset.transform import MedicalTransform
 from loss import GeneralizedDiceLoss
 from loss.util import class2one_hot
 from network import DenseUNet2D
@@ -47,51 +47,48 @@ from utils.vis import imshow
                                     'Recommend num_gpu in Linux',
               type=int, default=0, show_default=True)
 def main(epoch_num, batch_size, lr, num_gpu, data_path, log_path, resume, eval_intvl, cp_intvl, vis_intvl, num_workers):
-    # prepare
     data_path = Path(data_path)
     log_path = Path(log_path)
     cp_path = log_path / 'checkpoint'
-
+    
     if not resume and log_path.exists() and len(list(log_path.glob('*'))) > 0:
         print(f'log path "{str(log_path)}" has old file', file=sys.stderr)
         sys.exit(-1)
     if not cp_path.exists():
         cp_path.mkdir(parents=True)
-
-    train_transform = Compose([
-        MedicalTransform2(output_size=512, type='train')
-    ])
-    valid_transform = Compose([
-        MedicalTransform2(output_size=512, type='valid')
-    ])
-    dataset = KiTS19_roi(data_path, stack_num=3,
-                         train_transform=train_transform,
-                         valid_transform=valid_transform,
-                         spec_classes=[0, 1, 2])
-
-    net = DenseUNet2D(out_ch=dataset.num_classes)
-
+    
+    transform = MedicalTransform(output_size=512, roi_error_range=15, use_roi=True)
+    
+    dataset = KiTS19(data_path, stack_num=3, spec_classes=[0, 1, 2], img_size=(512, 512),
+                     use_roi=True, roi_file='roi.json', roi_error_range=5,
+                     train_transform=transform, valid_transform=transform)
+    
+    net = DenseUNet2D(in_ch=dataset.img_channels, out_ch=dataset.num_classes)
+    
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-
+    
     start_epoch = 0
     if resume:
+        data = {
+            'net': net,
+            'optimizer': optimizer,
+            'epoch': 0
+        }
         cp_file = Path(resume)
-        net, optimizer, start_epoch = cp.load_params(net, optimizer, root=str(cp_file))
-
-    # weights = np.array([0.2, 1.2, 2.2], dtype=np.float32)
-    # weights = torch.from_numpy(weights)
-    weights = None
+        cp.load_params(data, cp_file, device='cpu')
+        start_epoch = data['epoch'] + 1
+    
     criterion = GeneralizedDiceLoss(idc=[0, 1, 2])
-
+    
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode='min', factor=0.1, patience=5, verbose=True,
         threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08
     )
-
+    
     logger = SummaryWriter(str(log_path))
-
+    
     gpu_ids = [i for i in range(num_gpu)]
-
+    
     print(f'{" Start training ":-^40s}\n')
     msg = f'Net: {net.__class__.__name__}\n' + \
           f'Dataset: {dataset.__class__.__name__}\n' + \
@@ -100,9 +97,9 @@ def main(epoch_num, batch_size, lr, num_gpu, data_path, log_path, resume, eval_i
           f'Batch size: {batch_size}\n' + \
           f'Device: cuda{str(gpu_ids)}\n'
     print(msg)
-
+    
     torch.cuda.empty_cache()
-
+    
     # to GPU device
     net = torch.nn.DataParallel(net, device_ids=gpu_ids).cuda()
     criterion = criterion.cuda()
@@ -110,161 +107,164 @@ def main(epoch_num, batch_size, lr, num_gpu, data_path, log_path, resume, eval_i
         for k, v in state.items():
             if isinstance(v, torch.Tensor):
                 state[k] = v.cuda()
-
+    
     # start training
     valid_score = 0.0
     best_score = 0.0
     best_epoch = 0
-
+    
     for epoch in range(start_epoch, epoch_num):
         epoch_str = f' Epoch {epoch + 1}/{epoch_num} '
         print(f'{epoch_str:-^40s}')
-
-        lr = optimizer.param_groups[0]['lr']
-        print(f'Learning rate: {lr}')
-
+        print(f'Learning rate: {optimizer.param_groups[0]["lr"]}')
+        
         net.train()
         torch.set_grad_enabled(True)
+        transform.train()
         try:
-            loss = training(net, dataset, criterion, optimizer, scheduler, batch_size, num_workers, vis_intvl, logger,
-                            epoch)
-
+            loss = training(net, dataset, criterion, optimizer, scheduler, epoch, batch_size, num_workers, vis_intvl,
+                            logger)
+            
             if eval_intvl > 0 and (epoch + 1) % eval_intvl == 0:
                 net.eval()
                 torch.set_grad_enabled(False)
-
-                train_score = evaluation(net, dataset, batch_size, num_workers, vis_intvl, logger, epoch, type='train')
-                valid_score = evaluation(net, dataset, batch_size, num_workers, vis_intvl, logger, epoch, type='valid')
-
+                transform.eval()
+                
+                train_score = evaluation(net, dataset, epoch, batch_size, num_workers, vis_intvl, logger, type='train')
+                valid_score = evaluation(net, dataset, epoch, batch_size, num_workers, vis_intvl, logger, type='valid')
+                
                 print(f'Train data score: {train_score:.5f}')
                 print(f'Valid data score: {valid_score:.5f}')
-
+            
+            if valid_score > best_score:
+                best_score = valid_score
+                best_epoch = epoch
+                cp_file = cp_path / 'best.pth'
+                cp.save(epoch, net.module, optimizer, str(cp_file))
+                print('Update best acc!')
+                logger.add_scalar('best epoch', best_epoch + 1, 0)
+                logger.add_scalar('best score', best_score, 0)
+            
+            if (epoch + 1) % cp_intvl == 0:
+                cp_file = cp_path / f'cp_{epoch + 1:03d}.pth'
+                cp.save(epoch, net.module, optimizer, str(cp_file))
+            
+            print(f'Best epoch: {best_epoch + 1}')
+            print(f'Best score: {best_score:.5f}')
+        
         except KeyboardInterrupt:
             cp_file = cp_path / 'INTERRUPTED.pth'
             cp.save(epoch, net.module, optimizer, str(cp_file))
             return
 
-        if valid_score > best_score:
-            best_score = valid_score
-            best_epoch = epoch
-            cp_file = cp_path / 'best.pth'
-            cp.save(epoch, net.module, optimizer, str(cp_file))
-            print('Update best acc!')
-            logger.add_scalar('best epoch', best_epoch + 1, 0)
-            logger.add_scalar('best score', best_score, 0)
 
-        if (epoch + 1) % cp_intvl == 0:
-            cp_file = cp_path / f'cp_{epoch + 1:03d}.pth'
-            cp.save(epoch, net.module, optimizer, str(cp_file))
-
-        print(f'Best epoch: {best_epoch + 1}')
-        print(f'Best score: {best_score:.5f}')
-
-
-def training(net, dataset, criterion, optimizer, scheduler, batch_size, num_workers, vis_intvl, logger, epoch):
+def training(net, dataset, criterion, optimizer, scheduler, epoch, batch_size, num_workers, vis_intvl, logger):
     sampler = RandomSampler(dataset.train_dataset)
-
+    
     train_loader = DataLoader(dataset.train_dataset, batch_size=batch_size, sampler=sampler,
                               num_workers=num_workers, pin_memory=True)
-
+    
     tbar = tqdm(train_loader, ascii=True, desc='train', dynamic_ncols=True)
-    for batch_idx, (imgs, labels, idx) in enumerate(tbar):
-        optimizer.zero_grad()
-
-        imgs, labels = imgs.cuda(), labels.cuda()
-        feat, outputs, up1_cls, up2_cls, up3_cls, up4_cls = net(imgs)
-
-        losses = []
-        for up_outputs in [up1_cls, up2_cls, up3_cls, up4_cls]:
+    for batch_idx, data in enumerate(tbar):
+        imgs, labels = data['image'].cuda(), data['label'].cuda()
+        outputs = net(imgs)
+        
+        losses = {}
+        for key, up_outputs in outputs.items():
             b, c, h, w = up_outputs.shape
             up_labels = torch.unsqueeze(labels.float(), dim=1)
             up_labels = F.interpolate(up_labels, size=(h, w), mode='bilinear')
             up_labels = torch.squeeze(up_labels, dim=1).long()
+            
             up_labels_onehot = class2one_hot(up_labels, 3)
             up_outputs = F.softmax(up_outputs, dim=1)
-            losses.append(criterion(up_outputs, up_labels_onehot))
-
-        labels_onehot = class2one_hot(labels, 3)
-        outputs = F.softmax(outputs, dim=1)
-        losses.append(criterion(outputs, labels_onehot))
+            up_loss = criterion(up_outputs, up_labels_onehot)
+            losses[key] = up_loss
         
-        loss = sum(losses)
+        loss = sum(losses.values())
+        
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        
         if vis_intvl > 0 and batch_idx % vis_intvl == 0:
-            outputs = outputs.cpu().detach().numpy().argmax(axis=1)
-            imgs, labels, outputs = dataset.vis_transform(imgs, labels, outputs)
-            imshow(title='Train', imgs=(imgs[0][1], labels[0], outputs[0]), shape=(1, 3),
+            data['predict'] = outputs['output']
+            data = dataset.vis_transform(data)
+            imgs, labels, predicts = data['image'], data['label'], data['predict']
+            imshow(title='Train', imgs=(imgs[0, 1], labels[0], predicts[0]), shape=(1, 3),
                    subtitle=('image', 'label', 'predict'))
-
-        tbar.set_postfix(up1=f'{losses[0].item():.5f}', up2=f'{losses[1].item():.5f}', up3=f'{losses[2].item():.5f}',
-                         up4=f'{losses[3].item():.5f}', up5=f'{losses[4].item():.5f}', loss=f'{loss.item():.5f}')
-
+        
+        losses['total'] = loss
+        for k in losses.keys(): losses[k] = losses[k].item()
+        tbar.set_postfix(losses)
+    
     scheduler.step(loss.item())
-
-    for i in range(len(losses)):
-        logger.add_scalar(f'loss/up{i + 1}', losses[i].item(), epoch)
-    logger.add_scalar(f'loss/total', loss.item(), epoch)
+    
+    for k, v in losses.items():
+        logger.add_scalar(f'loss/{k}', v, epoch)
+        
     return loss.item()
 
 
-def evaluation(net, dataset, batch_size, num_workers, vis_intvl, logger, epoch, type):
+def evaluation(net, dataset, epoch, batch_size, num_workers, vis_intvl, logger, type):
     type = type.lower()
     if type == 'train':
         subset = dataset.train_dataset
-        case = dataset.case_indices[:dataset.split_case]
+        case_slice_indices = dataset.train_case_slice_indices
     elif type == 'valid':
         subset = dataset.valid_dataset
-        case = dataset.case_indices[dataset.split_case - 1:]
-
-    vol_case_i = 0
-    vol_label = []
-    vol_output = []
-
+        case_slice_indices = dataset.valid_case_slice_indices
+    
     sampler = SequentialSampler(subset)
     data_loader = DataLoader(subset, batch_size=batch_size, sampler=sampler,
                              num_workers=num_workers, pin_memory=True)
-
     evaluator = Evaluator(dataset.num_classes)
-
-    with tqdm(total=len(case) - 1, ascii=True, desc=f'eval/{type:5}', dynamic_ncols=True) as pbar:
-        for batch_idx, (imgs, labels, idx) in enumerate(data_loader):
-            imgs = imgs.cuda()
-            feat, outputs, _, _, _, _ = net(imgs)
+    
+    case = 0
+    vol_label = []
+    vol_output = []
+    
+    with tqdm(total=len(case_slice_indices) - 1, ascii=True, desc=f'eval/{type:5}', dynamic_ncols=True) as pbar:
+        for batch_idx, data in enumerate(data_loader):
+            imgs, labels, idx = data['image'].cuda(), data['label'], data['index']
+            
+            outputs = net(imgs)
+            outputs = outputs['output']
             outputs = outputs.argmax(dim=1)
-
-            np_labels = labels.cpu().detach().numpy()
-            np_outputs = outputs.cpu().detach().numpy()
+            
+            labels = labels.cpu().detach().numpy()
+            outputs = outputs.cpu().detach().numpy()
             idx = idx.numpy()
-
-            vol_label.append(np_labels)
-            vol_output.append(np_outputs)
-
-            while vol_case_i < len(case) - 1 and idx[-1] >= case[vol_case_i + 1] - 1:
+            
+            vol_label.append(labels)
+            vol_output.append(outputs)
+            
+            while case < len(case_slice_indices) - 1 and idx[-1] >= case_slice_indices[case + 1] - 1:
                 vol_output = np.concatenate(vol_output, axis=0)
                 vol_label = np.concatenate(vol_label, axis=0)
-
-                vol_idx = case[vol_case_i + 1] - case[vol_case_i]
+                
+                vol_idx = case_slice_indices[case + 1] - case_slice_indices[case]
                 evaluator.add(vol_output[:vol_idx], vol_label[:vol_idx])
-
+                
                 vol_output = [vol_output[vol_idx:]]
                 vol_label = [vol_label[vol_idx:]]
-                vol_case_i += 1
+                case += 1
                 pbar.update(1)
-
+            
             if vis_intvl > 0 and batch_idx % vis_intvl == 0:
-                imgs, labels, outputs = dataset.vis_transform(imgs, labels, outputs)
-                imshow(title='Train', imgs=(imgs[0][1], labels[0], outputs[0]), shape=(1, 3),
+                data['predict'] = outputs
+                data = dataset.vis_transform(data)
+                imgs, labels, predicts = data['image'], data['label'], data['predict']
+                imshow(title=f'eval/{type:5}', imgs=(imgs[0, 1], labels[0], predicts[0]), shape=(1, 3),
                        subtitle=('image', 'label', 'predict'))
-
+    
     acc = evaluator.eval()
-
+    
     for k in sorted(list(acc.keys())):
         if k == 'dc_each_case': continue
         print(f'{type}/{k}: {acc[k]:.5f}')
         logger.add_scalar(f'{type}/{k}', acc[k], epoch)
-
+    
     for i in range(len(acc['dc_each_case'])):
         dc_each_case = acc['dc_each_case'][i]
         for j in range(len(dc_each_case)):
@@ -274,7 +274,7 @@ def evaluation(net, dataset, batch_size, num_workers, vis_intvl, logger, epoch, 
             elif type == 'valid':
                 case_idx = dataset.valid_case[i]
             logger.add_scalar(f'{type}_each_case/{case_idx:05d}/dc_{j}', dc, epoch)
-
+    
     score = (acc['dc_per_case_1'] + acc['dc_per_case_2']) / 2
     logger.add_scalar(f'{type}/score', score, epoch)
     return score
